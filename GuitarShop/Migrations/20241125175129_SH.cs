121e540a09ea67a8a55d9d8d05fe797d87bc7c3f
samharris0509 <156223013+samharris0509@users.noreply.github.com>
2024-11-25 12:04:16 -0600

    Add project files.

---
+++ GuitarShop/Migrations/20241125175129_SH.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuitarShop.Migrations
+{
+    public partial class SH : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
